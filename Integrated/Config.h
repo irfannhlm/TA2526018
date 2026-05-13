67f99d5d706ac553586c8daf24feccf867ef9cb8
Syaugi <161228034+Mrdx77@users.noreply.github.com>
2026-05-13 12:31:29 +0700

    Add files via upload

---
+++ Integrated/Config.h
@@ -0,0 +1,71 @@
+// Config.h
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// I2C Pins (Shared: LCD, MPU6050, PN532)
+#define I2C_SDA 21
+#define I2C_SCL 22
+
+// I2S Pins (INMP441)
+#define I2S_WS 26
+#define I2S_SD 27
+#define I2S_SCK 25
+
+// SPI Pins (SD Card Module)
+#define SD_SCK 18
+#define SD_MISO 19
+#define SD_MOSI 23
+#define SD_CS 5
+
+// Extra Pins
+#define MPU_INT_PIN 32 // External interrupt MPU6050
+#define BUZZER_PIN 13 // Pin untuk indikator audio
+#define BUTTON_PIN 4  // Button untuk switching mode
+#define LED_BATTERY 16
+#define LED_WIFI    17
+
+// Pin untuk tombol reset WiFi (Tombol BOOT bawaan ESP32)
+#define RESET_BUTTON 0
+
+// Audio & SD Config
+#define SAMPLE_RATE       16000 
+#define I2S_READ_LEN      1024 
+#define BIT_SHIFT         14    
+#define SILENCE_THRESHOLD 300   
+#define SILENCE_LIMIT_MS  3000
+#define MAX_DURASI_DETIK  300   
+#define PRE_BUFFER_SEC    1.5     
+#define PRE_BUFFER_SIZE   (SAMPLE_RATE * PRE_BUFFER_SEC)
+#define MAX_RECORD_SECONDS 300
+#define MAX_RECORD_MS (MAX_RECORD_SECONDS * 1000UL)
+#define DOSEN_COUNTDOWN 8 // Jeda 8 detik
+#define TIMEOUT_BICARA    60000
+
+// Accelerometer Config
+#define FLYING_THRESHOLD 0.35f  // Di bawah 0.35g dianggap dilempar
+#define FLYING_DURATION_MS 100 // Minimal durasi melayang dalam ms
+
+// MQTT CONFIG
+#define MQTT_PORT 8883           
+#define DEVICE_ID "2"
+#define MQTT_SERVER "c4bbf4787735464dadc96ca13e4a9c6b.s1.eu.hivemq.cloud"
+#define MQTT_USER "catchnote"
+#define MQTT_PASS "Ta2526018"
+#define MQTT_TOPIC_REG         "kelas/alat/rfid"
+#define MQTT_TOPIC_STATUS      "kelas/alat/status"
+#define MQTT_TOPIC_PERINTAH    "kelas/alat/perintah"
+#define MQTT_TOPIC_AUDIO_DATA  "kelas/alat/audio_data"
+#define MQTT_TOPIC_SYNC_STATUS "kelas/alat/sync_status"
+
+// Wifi & MQTT Management
+extern char eap_nim[40];
+extern char eap_pass[40];
+extern char saved_ssid[40];
+extern char wifi_type[20];
+extern char wifi_pass[64];
+
+// Battery Status
+extern float batteryPercent;
+
+
+#endif
